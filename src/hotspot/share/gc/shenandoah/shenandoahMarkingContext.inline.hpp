a8e500db8c02bde78dc976847e2b417164943923
dchuyko <dmitry.chuyko@bell-sw.com>
2026-02-07 01:14:56 +0500

    Fix another merge typo

--- src/hotspot/share/gc/shenandoah/shenandoahMarkingContext.inline.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahMarkingContext.inline.hpp
@@ -83,6 +83,7 @@ inline HeapWord* ShenandoahMarkingContext::get_next_marked_addr(const HeapWord* 
 
 inline HeapWord* ShenandoahMarkingContext::get_last_marked_addr(const HeapWord* start, const HeapWord* limit) const {
   return _mark_bit_map.get_last_marked_addr(start, limit);
+}
 
 inline HeapWord* ShenandoahMarkingContext::get_prev_marked_addr(const HeapWord* limit, const HeapWord* start) const {
   return _mark_bit_map.get_prev_marked_addr(limit, start);
